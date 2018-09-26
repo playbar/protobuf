06699d58e12b09dafdc4b5526b70475043d24e65
playbar <hgl868@126.com>
2018-09-26 11:21:34 +0800

    添加mytest工程测试

--- cmake/protobuf-options.cmake
+++ cmake/protobuf-options.cmake
@@ -3,5 +3,5 @@ option(protobuf_VERBOSE "Enable for verbose output" OFF)
 mark_as_advanced(protobuf_VERBOSE)
 
 # FindProtobuf module compatibel
-option(protobuf_MODULE_COMPATIBLE "CMake build-in FindProtobuf.cmake module compatible" OFF)
+option(protobuf_MODULE_COMPATIBLE "CMake build-in FindProtobuf.cmake module compatible" ON)
 mark_as_advanced(protobuf_MODULE_COMPATIBLE)
